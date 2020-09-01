da76528951c17d5d6c895779c708f5ff31a69b1c
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-08-31 22:23:26 -0700

    got launch.json to work for 1 file

--- CSCI210/w2l1/GcdRunner.java
+++ CSCI210/w2l1/GcdRunner.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class GcdRunner {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("GCD");
         System.out.println("Code by Raj Pabari");
         GCD gcd = new GCD(24, 9);
         System.out.println(gcd);
         System.out.println("result = " + gcd.findGCD());
 
-        gcd = new GCD(4, 2);
+        System.out.println("Gimme 2 ints: ");
+        int int1 = scan.nextInt();
+        int int2 = scan.nextInt();
+        gcd = new GCD(int1, int2);
         System.out.println(gcd);
         System.out.println("result = " + gcd.findGCD());
     }
