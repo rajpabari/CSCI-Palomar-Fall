7e9cf42d022105af8eb3fbbb721f0f438be629f9
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-10-09 09:28:38 -0700

    kinda working

--- CSCI222/asmt5/DocumentReader.cpp
+++ CSCI222/asmt5/DocumentReader.cpp
@@ -7,31 +7,47 @@
 
 using namespace std;
 
-int searchVector(vector<char *> *words, char **word) {
+int searchVector(const vector<char *> *words, string word) {
+  char **myWord;
   for (int i = 0; i < words->size(); i++) {
-    if (strcmp(*word, (*words)[i]) == 0) {
-      words->push_back((*words)[i]);
+    if (word == (*words)[i]) {
+      myWord = &(*words)[i];
+      words->push_back(*myWord);
       return i;
     }
   }
-  words->push_back(*word);
+  char *newWord = new char[word.length() + 1];
+  strcpy(newWord, word.c_str());
+  words->push_back(newWord);
   return words->size() - 1;
 }
 
 int main() {
   vector<char *> words;
+  // vector<char **> document;
+  /*int a = 10;
+  int *p = &a;
+  int *q = p;
+  cout << *p << " " << *q << endl;
+  a = 11;
+  cout << *p << " " << *q << endl;*/
 
-  char *word = new char[MAX_LENGTH];
+  char *temp;
   int x;
+  string word;
   do {
-    word = new char[MAX_LENGTH];
     cin >> word;
-    searchVector(&words, &word);
-  } while (strcmp(word, "quit") != 0);
+    cout << searchVector(&words, word) << " ";
+  } while (word != "quit");
   words.pop_back();
 
   for (int i = 0; i < words.size(); i++) {
-    cout << &words[i] << " ";
+    cout << words[i] << " " << *words[i] << " " << &words[i] << endl;
+  }
+  words[0] = (char *)"jkjk";
+  cout << "\n\n";
+  for (int i = 0; i < words.size(); i++) {
+    cout << words[i] << " " << &words[i] << endl;
   }
   return 0;
 }
