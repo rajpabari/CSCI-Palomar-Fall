444371693314c92098bec71441bd25b36341866c
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-11-03 18:18:48 -0800

    first changes

---
+++ CSCI210/w11/LinkedListTester.java
@@ -0,0 +1,8 @@
+public class LinkedListTester {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.insertAtFront("red");
+        list.insertAtBack("green");
+        System.out.println(list);
+    }
+}
