6511e7c0ff92a59fd2efc638806b931319700ebf
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2022-01-25 15:42:35 -0800

    CSCI-210 renaming folders

--- CSCI210/ztest-project/newesttest.java
+++
@@ -1,6 +0,0 @@
-public class newesttest {
-
-    public static void main(String[] args) {
-        System.out.println("Hellooooo world");
-    }
-}
--- CSCI210/ztest-project/test.py
+++
@@ -1,9 +0,0 @@
-def main():
-    i = 10
-    for x in range(10):
-        i += 1
-    return i
-
-
-if __name__ == "__main__":
-    print(main())
